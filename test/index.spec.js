f68fe2510fa6bad5781d2677ca5000038b894620
Alã Damasceno <alacerdan@hotmail.com>
2021-11-07 19:57:41 -0300

    Add more tests

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,15 +1,39 @@
 const Packer = require('../src');
+const packer = new Packer()
 
-test('should pack object has property *minify*', () => {
-    const pack = new Packer()
-
-    expect(pack).toHaveProperty('minify');
-
+test('should packer object to be instance of Packer', () => {
+    expect(packer).toBeInstanceOf(Packer);
 });
 
-test('should pack object has property *pack*', () => {
-    const pack = new Packer()
+test('should packer object has property *minify*', () => {
+    expect(packer).toHaveProperty('minify');
+});
 
-    expect(pack).toHaveProperty('pack');
+test('should packer object has property *pack*', () => {
+    expect(packer).toHaveProperty('pack');
+});
 
+test('should return "string" received "number"', () => {
+    const received = packer.pack(1);
+    expect(typeof received).toBe('string')
+});
+
+test('should return "string" received "array"', () => {
+    const received = packer.pack([]);
+    expect(typeof received).toBe('string')
+});
+
+test('should return "string" received "object"', () => {
+    const received = packer.pack({});
+    expect(typeof received).toBe('string')
+});
+
+test('should return "1+2"', () => {
+    const received = packer.pack('1 + 2')
+    expect(received).toBe('1+2');
+});
+
+test('should return eval string containing "p,a,c,k,e,r"', () => {
+    const received = packer.pack('', true);
+    expect(received).toContain('p,a,c,k,e,r');
 });
